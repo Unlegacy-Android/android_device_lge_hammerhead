be4af54b26c6501787b2ad7ef58e4c9e206c14cb
Svyatoslav Ryhel <clamor95@gmail.com>
2018-04-23 18:32:18 +0300

    hammerhead: move proprietary and config files to /vendor
    Use the TARGET_COPY_OUT_VENDOR macro to ensure using the correct
    location. Use LOCAL_PROPRIETARY_MODULE to move building modules into
    vendor.
    
    Change-Id: I331c9700502ad7c08b470a0e2a03a1b3d3cb1c49
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- voice_processing/Android.mk
+++ voice_processing/Android.mk
@@ -26,7 +26,8 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_MODULE := libqcomvoiceprocessingdescriptors
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/soundfx
+LOCAL_MODULE_RELATIVE_PATH := soundfx
+LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
